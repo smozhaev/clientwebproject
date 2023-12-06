9e89a4a507910b7495ddf7c1b80b23ebb7776b8a
smozhaev <kmozhaev100@gmail.com>
2023-12-06 10:55:30 +0300

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const getStaff = async () => {
+    const data = await axios.get(
+      "https://e.mospolytech.ru/old/lk_api.php/?getStaff&search=&division=&page=50&perpage=50&token=encs7hdoQvvlRbnmdYMx6iYbN6BV4c0u3OFzrYnFLLnxMTZgPEFWacFk%2BbO2lmIVpB4FZl3gw4Gl4vqwmhv0ZozGywqMuZHzO9Dp7nzjoCq3RxTWE0Lh58dVG%2FAoV9M13ElJ6AUayD%2B9Vw41XTcwOFhTEtm5OaGaCnVtzkeGR28%3D"
+    );
+    return response.data;
+  };
+
+  useEffect(() => {
+    console.log(getStaff());
+  });
+
   return (
     <div className="App">
       <header className="App-header">
